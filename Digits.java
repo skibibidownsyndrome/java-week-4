fdd7319335aea26e11f88028ef23a6b12dae4fcf
skibibidownsyndrome <lupug922@gmail.com>
2025-02-26 09:50:20 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -8,17 +8,21 @@ public class Digits {
 public static void main(String[]args) {
 
     int number;
-    number = 258;
+    number = 2584;
     int ones;
     int tens;
     int hundreds;
-    ones = number % 10;
-    tens = (number / 10) % 10;
-    hundreds = (number / 10 / 10) % 10;
+    int thousands;
+    
+    thousands = number % 10;
+    hundreds = (number / 10) % 10;
+    tens = (number / 10 / 10) % 10;
+    ones = (number / 10 / 10 /10) % 10;
 
     System.out.println(ones);
     System.out.println(tens);
     System.out.println(hundreds);
+    System.out.println(thousands);
 
 
 
