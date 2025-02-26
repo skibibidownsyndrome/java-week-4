a09bea739256334d238038f71fa4f8710a8643ac
skibibidownsyndrome <lupug922@gmail.com>
2025-02-26 10:26:25 -0500

    Update Digits.java

--- Digits.java
+++ Digits.java
@@ -1,5 +1,7 @@
 //2/25/2025, George Lupu
 
+import java.util.*;
+
 public class Digits {
 
 
@@ -8,22 +10,26 @@ public class Digits {
 public static void main(String[]args) {
 
     int number;
-    number = 2584;
+    //number = 2584;
     int ones;
     int tens;
     int hundreds;
     int thousands;
     
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a 4 digit number");
+    number = input.nextInt();
+
     thousands = number % 10;
     hundreds = (number / 10) % 10;
     tens = (number / 10 / 10) % 10;
     ones = (number / 10 / 10 /10) % 10;
 
-    System.out.println(ones);
-    System.out.println(tens);
-    System.out.println(hundreds);
-    System.out.println(thousands);
-
+    System.out.println("The ones digit is " + ones);
+    System.out.println("The tens digit is " + tens);
+    System.out.println("The hundreds digit is " + hundreds);
+    System.out.println("The thousands digit is " + thousands);
 
 
     }
